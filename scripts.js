34a7c35d21c7cddea5a0ea4ed1e29e13d0c15c4e
nwshane <django09@gmail.com>
2014-09-05 23:52:44 +0400

    REFACTOR: Rewrote JS code using a module pattern to avoid global variables, to hide private code, and to expose public code.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,27 @@
-
-
-
-function initialize() {
+sim = function () {
     var canvas = document.getElementById( 'simulation' );
 
-    if (canvas.getContext) {
-        var ctx = canvas.getContext( '2d' );
-        ctx.canvas.width = window.innerWidth;
-        ctx.canvas.height = window.innerHeight;
-        w = ctx.canvas.width;
-        h = ctx.canvas.height;
-    } else {
-        // canvas unsupported code here
+    // If canvas is unsupported...
+    if ( !canvas.getContext ) {
+        return {
+            // canvas unsupported code here
+        }
     }
-}
 
-function draw() {
-    var canvas = document.getElementById( 'simulation' );
     var ctx = canvas.getContext( '2d' );
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    var w = ctx.canvas.width;
+    var h = ctx.canvas.height;
 
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect( .25 * w,.25 * h,.3 * w,.3 * h );
+    return {
+        draw: function() {
+            ctx.fillStyle = "rgb(200,0,0)";
+            ctx.fillRect( .25 * w, .25 * h, .3 * w, .3 * h );
 
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect( .75 * w,.75 * h,-.3 * w,-.3 * h );
-}
+            ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+            ctx.fillRect( .75 * w, .75 * h, -.3 * w, -.3 * h );
+        }
+    }
+}();
 
