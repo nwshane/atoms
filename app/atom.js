2602dc33057f554073cb3138a0735726db02b19b
nwshane <django09@gmail.com>
2014-09-11 08:52:46 +0400

    Atoms now bounce off all walls.

--- app/atom.js
+++ app/atom.js
@@ -48,14 +48,27 @@ atom = function() {
             if ( atom.x - atom.radius < 0 ) {
                 atom.direction = Math.PI - atom.direction;
             }
+
+            // Right wall
+            if ( atom.x + atom.radius > sim.getW() ) {
+                atom.direction = Math.PI - atom.direction;
+            }
+
+            // Top wall
+            if ( atom.y - atom.radius < 0 ) {
+                atom.direction = -1 * atom.direction;
+            }
+
+            // Bottom wall
+            if ( atom.y + atom.radius > sim.getH() ) {
+                atom.direction = -1 * atom.direction;
+            }
         },
         collide: function() {
             for (var i = 0; i < atoms.length; i++ ) {
                 var atom1 = atoms[i]
 
                 this.collideWall( atom1 );
-
-
             }
         },
         moveAtoms: function() {
