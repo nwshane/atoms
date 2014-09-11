77842e9748680f047c949edd0e2087c464f59ef3
nwshane <django09@gmail.com>
2014-09-11 16:18:15 +0400

    Prints to console when atoms overlap.

--- app/atom.js
+++ app/atom.js
@@ -1,8 +1,4 @@
 atom = function() {
-    function random( min, max ) {
-        return (Math.random() * (1 + max - min)) + min;
-    }
-
     var atoms = [];
 
     var minRadius = 20;
@@ -11,12 +7,17 @@ atom = function() {
     var minSpeed = 50;
     var maxSpeed = 150;
 
+    function random( min, max ) {
+        return (Math.random() * (1 + max - min)) + min;
+    }
+
     return {
         getAtoms: function() {
             return atoms;
         },
         Atom: function() {
             this.radius = random( minRadius, maxRadius );
+            this.mass = Math.PI * this.radius * this.radius;
             this.color = "rgb(0,0,0)";
             this.speed = random ( minSpeed, maxSpeed );
             this.direction = random( 0, 2 * Math.PI );
@@ -35,19 +36,25 @@ atom = function() {
                 ctx.fillStyle = this.color;
                 ctx.arc( this.x, this.y, this.radius, 0, 2 * Math.PI, true );
                 ctx.fill();
-            }
+            };
 
             this.move = function() {
                 var intervalLengthMs = sim.getIntervalLengthMs();
                 this.x += intervalLengthMs/1000 * this.speed * Math.cos( this.direction );
                 this.y += intervalLengthMs/1000 * this.speed * Math.sin( this.direction );
-            }
+            };
         },
         moveAtoms: function() {
             for ( var i = 0; i < atoms.length; i++ ) {
                 atoms[i].move();
                 atoms[i].draw();
             }
+        },
+        distance: function( atom1, atom2 ) {
+            var xDist = atom1.x - atom2.x;
+            var yDist = atom1.y - atom2.y;
+
+            return Math.sqrt( xDist * xDist + yDist * yDist );
         }
     }
 }();
--- app/collide.js
+++ app/collide.js
@@ -1,33 +1,52 @@
+//# sourceURL=collide.js
+
 collide = function() {
-    function collideWall( atom ) {
+    function collideWall( atom1 ) {
         // Left wall
-        if ( atom.x - atom.radius < 0 ) {
-            atom.direction = Math.PI - atom.direction;
+        if ( atom1.x - atom1.radius < 0 ) {
+            atom1.direction = Math.PI - atom1.direction;
         }
 
         // Right wall
-        if ( atom.x + atom.radius > sim.getW() ) {
-            atom.direction = Math.PI - atom.direction;
+        if ( atom1.x + atom1.radius > sim.getW() ) {
+            atom1.direction = Math.PI - atom1.direction;
         }
 
         // Top wall
-        if ( atom.y - atom.radius < 0 ) {
-            atom.direction = -1 * atom.direction;
+        if ( atom1.y - atom1.radius < 0 ) {
+            atom1.direction = -1 * atom1.direction;
         }
 
         // Bottom wall
-        if ( atom.y + atom.radius > sim.getH() ) {
-            atom.direction = -1 * atom.direction;
+        if ( atom1.y + atom1.radius > sim.getH() ) {
+            atom1.direction = -1 * atom1.direction;
         }
+    }
+
+    function checkAtomOverlap( atom1, atom2 ) {
+        return (( atom1.radius + atom2.radius ) > atom.distance( atom1, atom2 ));
+    }
+
+    function atomCollision( atom1, atom2 ) {
+
     }
 
     return {
         collide: function() {
             var atoms = atom.getAtoms();
 
-            for (var i = 0; i < atoms.length; i++ ) {
-                var atom1 = atoms[i]
+            for ( var i = 0; i < atoms.length; i++ ) {
+                var atom1 = atoms[i];
+
                 collideWall( atom1 );
+
+                // Compare the location of every atom to every other atom to check whether two atoms are colliding
+                for ( var j = i + 1; j < atoms.length; j++ ) {
+                    var atom2 = atoms[j];
+                    if ( checkAtomOverlap( atom1, atom2 ) ) {
+                        atomCollision( atom1, atom2 );
+                    }
+                }
             }
         }
     }
