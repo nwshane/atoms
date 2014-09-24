e8a089ee650915482531a0d231d398869cd0e06e
nwshane <django09@gmail.com>
2014-09-24 17:27:56 +0400

    Panels and headers now fade out and return with mouse movement.

--- app/fader.js
+++ app/fader.js
@@ -1,10 +1,27 @@
 define([ 'jquery' ], function( $ ){
-    var fadeOutSpeed = 2000;
+    var elements = ['hgroup', '#panels'];
+
+    var fadeOutSpeed = 1000;
 
     function fade() {
-        $('hgroup').fadeOut( fadeOutSpeed );
-        $('#panels').fadeOut( fadeOutSpeed );
+        setTimeout( function(){
+            $.each( elements, function( index, element ) {
+                $( element ).fadeOut(fadeOutSpeed);
+            });
+        }, 1000)
+
     }
 
+    $('body').mousemove( function() {
+        $.each( elements, function( index, element ) {
+            $(element).stop( true );
+            $(element).css('display', '');
+            $(element).css('opacity', '');
+        })
+    });
+
+    $('canvas').mousemove()
+
     fade();
+
 });
