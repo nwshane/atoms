6956f84e918762bb1c80cdac69c43585fecbb947
nwshane <django09@gmail.com>
2014-09-30 18:51:05 +0400

    jQuery plugs in atom display for each atom.

--- app/display.js
+++ app/display.js
@@ -9,12 +9,17 @@ define([ 'jquery', 'atom' ], function( $, atom ) {
     }
 
     function update() {
-        var propertiesToUpdate = [ 'id', 'radius', 'x', 'y', 'direction', 'speed' ]
+        var propertiesToUpdate = [ 'id', 'radius', 'x', 'y', 'direction', 'speed' ];
         var atoms = atom.getAtoms();
 
         // Update display with properties of selected atom
         for ( var i = 0; i < atoms.length; i++ ) {
             if ( atoms[i].selected ) {
+                // If atom's display does not exist yet, create it
+                if (!($('#display-atom-' + atoms[i].id).length)) {
+                    $('#display').append('<div id="display-atom-' + atoms[i].id + '"><h3>ATOM #<span id="selected-atom-id"></span></h3><p>Radius: <span id="selected-atom-radius"></span></p><p>Coordinates: (<span id="selected-atom-x"></span>, <span id="selected-atom-y"></span>)</p><p>Direction: <span id="selected-atom-direction"></span>&deg;</p><p>Speed: <span id="selected-atom-speed"></span> px/s</p></div>');
+                }
+
                 for (var j = 0; j < propertiesToUpdate.length; j++ ) {
                     updateDisplayProperty( atoms[i], propertiesToUpdate[j] )
                 }
@@ -23,11 +28,11 @@ define([ 'jquery', 'atom' ], function( $, atom ) {
     }
 
     return {
-        selectAtomById: function(id) {
-            atom.selectAtomById(id);
+        update: function() {
             update();
         },
-        update: function() {
+        selectAtomById: function(id) {
+            atom.selectAtomById(id);
             update();
         }
     };
