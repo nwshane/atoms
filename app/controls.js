cef7cba9a3379824e37c9ce6d28336ec1ee68d51
nwshane <django09@gmail.com>
2014-11-28 15:42:11 +0400

    Replaced play control images with higher quality versions of the same images.

--- app/controls.js
+++ app/controls.js
@@ -58,10 +58,10 @@ define([ 'jquery', 'hotkeys', 'sim', 'atom', 'collide', 'display', 'input', 'tog
         var $button = $('#play-pause');
         if (playing) {
             beginInterval();
-            $button.html('<img src="assets/pause.gif" alt="Play/Pause Button">');
+            $button.html('<img src="assets/pause.png" alt="Play/Pause Button">');
         } else {
             stopInterval();
-            $button.html('<img src="assets/play.gif" alt="Play/Pause Button">');
+            $button.html('<img src="assets/play.png" alt="Play/Pause Button">');
         }
     }
 
